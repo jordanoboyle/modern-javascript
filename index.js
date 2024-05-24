138d452fa623fa4bf463bab504c4c99f24046f66
jordanoboyle <jmcmcode@gmail.com>
2024-05-24 13:12:18 -0600

    review of babel

--- index.js
+++ index.js
@@ -21,3 +21,29 @@ console.log(newDate);
 
 let czechDate = moment().format('LLL');
 console.log(czechDate);
+
+import { sqrt } from 'mathjs';
+
+console.log(sqrt(-4).toString()); // 2i
+console.log(sqrt(4).toString()) // 2
+console.log(sqrt(-4).toString()) // NaN
+
+spaceBar();
+
+let evens = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var odds = evens.map(v => v + 1);
+var nums = evens.map((v, i) => v + i);
+
+console.log(odds);
+console.log(evens);
+
+let menuItems = {
+  item1: {food: "cordon bleu", price: 35},
+  item2: {food: "Bistro burger", price: 25}
+};
+console.log(`Ordered the ${menuItems.item2.food}. Total: ${menuItems.item2.price}.`);
+console.log(`Ordered the ${menuItems.item1.food}. Total: ${menuItems.item1.price}.`);
+
+console.log("This is new");
+
+console.log("This is newer");
